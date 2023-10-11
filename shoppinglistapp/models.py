29a33ed9e4a3ac73e6953635c794a30a5165bf3a
slbabin <>
2023-10-11 19:07:08 +0000

    Created model for the app and the superuser account

--- shoppinglistapp/models.py
+++ shoppinglistapp/models.py
@@ -1,5 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
+class Item(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=100)
+    notes = models.TextField(null=True, blank=True)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.name
 
+    class Meta:
+        ordering = ['complete']

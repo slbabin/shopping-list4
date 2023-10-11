6dfb2a25208adab1040d26513ec5929113c8e977
slbabin <>
2023-10-11 20:30:02 +0000

    Add Detail class to the view. Create Item_detail.html template

--- shoppinglistapp/views.py
+++ shoppinglistapp/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Item
 
 
 class ShoppingList(ListView):
     model = Item
+    context_object_name = 'items'
+
+
+class ItemDetail(DetailView):
+    model = Item
+    
 

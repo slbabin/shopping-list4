8a1529fd0c95aa46a0fd8c02422f1ef55f59c551
slbabin <>
2023-10-13 19:03:40 +0000

    Added Item update view

--- shoppinglistapp/urls.py
+++ shoppinglistapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ShoppingList, ItemDetail, ItemCreate
+from .views import ShoppingList, ItemDetail, ItemCreate, ItemUpdate
 
 
 urlpatterns = [
     path('', ShoppingList.as_view(), name='shopping_list'),
     path('item/<int:pk>', ItemDetail.as_view(), name='item'),
-    path('item-create/', ItemCreate.as_view(), name='item-create')
+    path('item-create/', ItemCreate.as_view(), name='item-create'),
+    path('item-update/<int:pk>', ItemUpdate.as_view(), name='item-update')
 ]
--- shoppinglistapp/views.py
+++ shoppinglistapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
 
 from .models import Item
@@ -20,7 +20,11 @@ class ItemDetail(DetailView):
 class ItemCreate(CreateView):
     model = Item
     # fields = ('user', 'name', 'notes', 'complete')
-    fields = '__all__'  
-    success_url = '/'
-    # success_url = reverse_lazy('items')
+    fields = '__all__'
+    success_url = reverse_lazy('shopping_list')
 
+
+class ItemUpdate(UpdateView):
+    model = Item
+    fields = '__all__'
+    success_url = reverse_lazy('shopping_list')

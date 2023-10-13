d78200a10047a973a24589a9e3e34df2e1e4df89
slbabin <>
2023-10-13 19:50:21 +0000

    Create Item Delete view. Add authantication view

--- shoppinglistapp/urls.py
+++ shoppinglistapp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import ShoppingList, ItemDetail, ItemCreate, ItemUpdate
+from .views import ShoppingList, ItemDetail, ItemCreate, ItemUpdate, ItemDelete, AppLogin
 
 
 urlpatterns = [
+    path('login/', AppLogin, name='login'),
     path('', ShoppingList.as_view(), name='shopping_list'),
     path('item/<int:pk>', ItemDetail.as_view(), name='item'),
     path('item-create/', ItemCreate.as_view(), name='item-create'),
-    path('item-update/<int:pk>', ItemUpdate.as_view(), name='item-update')
+    path('item-update/<int:pk>', ItemUpdate.as_view(), name='item-update'),
+    path('item-delete/<int:pk>', ItemDelete.as_view(), name='item-delete')
 ]
--- shoppinglistapp/views.py
+++ shoppinglistapp/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
+from django.contrib.auth.views import LoginView
+
 from .models import Item
+
+
+class AppLogin(LoginView):
+    template_name = "shoppinglistapp/login.html"
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('shopping_list')
 
 
 class ShoppingList(ListView):
@@ -21,6 +32,12 @@ class ItemCreate(CreateView):
     model = Item
     # fields = ('user', 'name', 'notes', 'complete')
     fields = '__all__'
+    success_url = reverse_lazy('shopping_list')
+
+
+class ItemDelete(DeleteView):
+    model = Item
+    context_object_name = 'item'
     success_url = reverse_lazy('shopping_list')
 
 

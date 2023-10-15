e9b7b20dfbf4188b230368c811f8e28794f1792b
Slava B <slbabin@gmail.com>
2023-10-15 19:02:17 +0000

    Create register user view. Create register template and linked to the regiser view

--- shoppinglistapp/views.py
+++ shoppinglistapp/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
 
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 from .models import Item
 
@@ -17,6 +19,13 @@ class AppLogin(LoginView):
 
     def get_success_url(self):
         return reverse_lazy('shopping_list')
+
+
+class PageRegister(FormView):
+    template_name = 'shoppinglistapp/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('shopping_list')
 
 
 class ShoppingList(LoginRequiredMixin, ListView):
